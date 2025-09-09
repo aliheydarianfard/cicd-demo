a7ca6ba985976fbda70bf754b88617adf59cb7a3
ali heydarianfard <alihf74@gmail.com>
2025-09-10 01:01:33 +0330

    add new summries

--- Web.Api/Controllers/WeatherForecastController.cs
+++ Web.Api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Web.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Ci-Cd"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
